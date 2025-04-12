053cd4ae6e9a8bc116b9b784b32154572b17fa15
Daniel Popp <dpopp783@gmail.com>
2025-04-11 22:40:37 -0600

    outline structure with components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,17 @@
+import Background from "./Background";
+import Container from "./Container";
+import Footer from "./Footer";
+import Header from "./Header";
+
 function App() {
-  return <>Hello!</>;
+  return (
+    <>
+      <Background></Background>
+      <Header></Header>
+      <Container></Container>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
